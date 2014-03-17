46d05b500aa5b2a7861f11be07ac177a4ae0c8a0
Danny Whalen <daniel.r.whalen@gmail.com>
2014-03-16 20:38:57 -0700

    Lazily execute the csv transformation

--- csv_transform.rb
+++ csv_transform.rb
@@ -1,23 +1,27 @@
 require 'csv'
+require 'memoizable'
 
 class CSVTransform
+  include Memoizable
+
   def initialize(*attributes)
     @attributes = attributes
   end
 
-  def call(user_list)
-    user_list.
-      map { |user| row_for(user) }. # map to array of attributes
-      unshift(headers).             # prepend the header row
-      map { |row| row.to_csv }.     # turn each row into csv data
-      join                          # join the rows into one blob
+  def call(list)
+    headers + list.
+      lazy.                       # lazily evaluate the list
+      map { |obj| row_for(obj) }. # map to array of attributes
+      map { |row| row.to_csv   }. # turn each row into csv data
+      force.                      # evaluate the lazy enumerable
+      join                        # join the rows into one blob
   end
 
-  def row_for(user)
-    @attributes.map { |attr| user.public_send(attr) }
+  def row_for(obj)
+    @attributes.map { |attr| obj.public_send(attr) }
   end
 
-  def headers
-    @attributes.map { |attr| attr.to_s.sub('_', ' ') }
+  memoize def headers
+    @attributes.map { |attr| attr.to_s.sub('_', ' ') }.to_csv
   end
 end
